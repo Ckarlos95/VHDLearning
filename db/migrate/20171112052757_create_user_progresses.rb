2a4d21c84e58074f49dcf85313fb69e2fd5c9700
Carlos Aguilera <ckarlos_95@hotmail.es>
2017-11-20 01:55:52 -0600

    Updated UserProgress model

--- db/migrate/20171112052757_create_user_progresses.rb
+++ db/migrate/20171112052757_create_user_progresses.rb
@@ -2,7 +2,7 @@ class CreateUserProgresses < ActiveRecord::Migration
   def change
     create_table :user_progresses do |t|
       t.references :user, index: true, foreign_key: true, null: false
-      t.references :topic, index: true, foreign_key: true, null: false #, default: 1
+      t.references :topic, index: true, foreign_key: true, null: false, default: 1
       t.integer :xp, default: 0
       t.integer :level, default: 1
 
